464dab8ea22d3522de9d4dd3fd13fa8d5e54eec5
Mykhailo Sehlianik <0000230000m@gmail.com>
2022-12-30 19:56:52 +0200

    Updated HelloWorld file

--- src/Main/Java/com/hillel/lessons/lesson1/HelloWorld.java
+++ src/Main/Java/com/hillel/lessons/lesson1/HelloWorld.java
@@ -2,6 +2,6 @@ package com.hillel.lessons.lesson1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello  World");
     }
 }
